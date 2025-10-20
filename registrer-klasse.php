f3799c5f8ae242be296d137c2dbe055463465c57
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-10-20 13:33:26 +0200

    Update registrer-klasse.php

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -1,1 +1,47 @@
+<?php  /* registrer klasse */
+?> 
 
+<h3>Registrer en ny klasse </h3>
+
+<form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
+  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+  Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
+  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" /> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
+<?php 
+  if (isset($_POST ["registrerKlasseKnapp"]))
+    {
+      $klassekode=$_POST ["klassekode"];
+      $klassenavn=$_POST ["klassenavn"];
+      $studiumkode=$_POST ["studiumkode"];
+
+      if (!$klassekode || !$klassenavn || !$studiumkode)
+        {
+          print ("klassekode, klassenavn og studiumkode m&aring; fylles ut");
+        }
+      else
+        {
+          include("db-tilkobling.php");  
+
+          $sqlSetning="SELECT * FROM klasse WHERE klassekode='$klassekode';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader!=0) 
+            {
+              print ("Klassen er registrert fra f&oslashr");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO klasse VALUES('$klassekode','$klassenavn','$studiumkode');";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
+               
+
+              print ("F&oslash;lgende klasse er n&aring; registrert: $klassekode $klassenavn $studiumkode"); 
+            }
+        }
+    }
+?> 
