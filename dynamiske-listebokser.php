06e6ae11db02fdced589c0a85043e023b992789b
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-10-22 13:07:55 +0200

    Create dynamiske-listebokser.php

---
+++ dynamiske-listebokser.php
@@ -0,0 +1,43 @@
+<?php  /*  dynamiske listebokser */
+
+
+function listeboksklassekode()
+{
+  include("db-tilkobling.php"); 
+      
+  $sqlSetning="SELECT * FROM klasse ORDER BY klassekode;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+   
+	
+  $antallRader=mysqli_num_rows($sqlResultat); 
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  
+      $klassekode=$rad["klassekode"]; 
+
+      print("<option value='$klassekode'>$klassekode</option>"); 
+    }
+}
+
+function listeboksbrukernavn()
+{
+  include("db-tilkobling.php");  
+      
+  $sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+  
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  
+      $brukernavn=$rad["brukernavn"]; 
+
+      print("<option value='$brukernavn'>$brukernavn</option>");  
+    }
+}
+
+
+?>
