e0d1b43621a454a597b7e590b7bad4a42e201fc9
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-10-22 12:43:38 +0200

    Update vis-alle-studenter.php

--- vis-alle-studenter.php
+++ vis-alle-studenter.php
@@ -1,1 +1,26 @@
+<?php  
+  include("db-tilkobling.php");
 
+  $sqlSetning="SELECT * FROM student;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+ 
+	
+  $antallRader=mysqli_num_rows($sqlResultat); 
+
+  print ("<h3>Registrerte studenter</h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th> <th align=left>etternavn</th> <th align=left>klassekode</th></tr>"); 
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  
+      $brukernavn=$rad["brukernavn"];        
+      $fornavn=$rad["fornavn"];   
+      $etternavn=$rad["etternavn"];
+      $klassekode=$rad["klassekode"];
+        
+      print ("<tr> <td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>");
+    }
+  print ("</table>"); 
+?>
