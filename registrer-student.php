4e5b2b33d2e6c0b77ea48317b17f7515cc685f0a
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-10-22 13:14:38 +0200

    Update registrer-student.php

--- registrer-student.php
+++ registrer-student.php
@@ -7,7 +7,10 @@
   Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
   Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
   Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
-  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  Klassekode <select name="klassekode" id="klassekode">
+    <option value="">velg klassekode</option>
+    <?php include("dynamiske-listebokser.php"); listeboksklassekode(); ?> 
+  </select>  <br/>
   <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
