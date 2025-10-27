2f1d3bc478165c59dee6ad974fb322c564e772cf
TYBW-Ichigo <65979633+TYBW-Ichigo@users.noreply.github.com>
2025-10-27 12:06:41 +0100

    Update db-tilkobling.php

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,9 +1,9 @@
 <?php  
 
-$host = getenv('DB_HOST', 'b-studentsql-1.usn.no');
-$username = getenv('DB_USER', 'chtal6548');
-$password = getenv('DB_PASSWORD', '2226chtal6548');
-$database = getenv('DB_DATABASE', 'chtal6548');
+$host = getenv('b-studentsql-1.usn.no');
+$username = getenv('chtal6548');
+$password = getenv('2226chtal6548');
+$database = getenv('chtal6548');
 
  $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
    
